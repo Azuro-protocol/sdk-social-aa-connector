b33580255a61aa5576a692613fd31d6b7d77cb8c
on47sky <code@onatsky.ru>
2025-01-25 17:50:17 +0300

    v1.1.1

--- src/FixSocialLogin.tsx
+++ src/FixSocialLogin.tsx
@@ -1,47 +1,18 @@
-import { useAccount, useAccountEffect } from 'wagmi'
-import { usePrivy, useWallets } from '@privy-io/react-auth'
-import { useSetActiveWallet } from '@privy-io/wagmi'
-import { useEffect, useLayoutEffect, useState } from 'react'
+import { useAccountEffect } from 'wagmi'
+import { usePrivy } from '@privy-io/react-auth'
 
 
 export default function FixSocialLogin() {
   const { authenticated, logout, user } = usePrivy()
-  const { ready, wallets } = useWallets()
-  const { setActiveWallet } = useSetActiveWallet()
-  const { address, isConnecting, isReconnecting } = useAccount()
-
-  const [ isProcessing, setProcessing ] = useState<boolean>(false)
 
   useAccountEffect({
     onDisconnect: () => {
       if (authenticated) {
-        setProcessing(true)
-
         logout()
-          .finally(() => {
-            setProcessing(false)
-          })
           .catch(() => {})
       }
     },
   })
-
-  useLayoutEffect(() => {
-    const wallet = wallets?.[0]
-
-    if (
-      wallet
-      && ready
-      && (!address || wallet.address.toLowerCase() !== address?.toLowerCase())
-      && !isProcessing
-      && !isConnecting
-      && !isReconnecting
-    ) {
-      setProcessing(true)
-      setActiveWallet(wallet)
-        .finally(() => setProcessing(false))
-    }
-  }, [ wallets, address, isProcessing, isConnecting, isReconnecting, ready ])
 
   return null
 }
