7fda30412f0dd646e885a004eb97a146d0fe31b4
on47sky <code@onatsky.ru>
2026-06-15 15:54:53 +0300

    v2.0.2 add useAAWalletClients to use instead of useAAWalletClient (re-export from useSmartWallets)

--- src/AAWalletClientProvider.tsx
+++ src/AAWalletClientProvider.tsx
@@ -1,18 +1,22 @@
 import React, { useContext, createContext } from 'react'
 import { useSmartWallets } from '@privy-io/react-auth/smart-wallets'
 
+export type AAWalletClients = ReturnType<typeof useSmartWallets>
 export type AAWalletClientProviderContextValue = ReturnType<typeof useSmartWallets>['client']
 export const AAWalletClientProviderContext = createContext<any>(undefined)
 
+/** @deprecated use `useAAWalletClients` instead */
 export const useAAWalletClient = () => {
   return useContext(AAWalletClientProviderContext) as AAWalletClientProviderContextValue
 }
+
+export const useAAWalletClients = useSmartWallets as () => AAWalletClients
 
 export const AAWalletClientProvider = ({ children }: { children: React.ReactNode }) => {
   const { client } = useSmartWallets()
 
   return (
-    <AAWalletClientProviderContext.Provider value={client as AAWalletClientProviderContextValue}>
+    <AAWalletClientProviderContext.Provider value={client}>
       {children}
     </AAWalletClientProviderContext.Provider>
   )
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { default as PrivyProvider, type PrivyConfig } from './Provider'
-export { useAAWalletClient, type AAWalletClientProviderContextValue, AAWalletClientProviderContext } from './AAWalletClientProvider'
+export { useAAWalletClient, type AAWalletClientProviderContextValue, AAWalletClientProviderContext, useAAWalletClients } from './AAWalletClientProvider'
 export { useAccount, type ExtendedAccountContextValue } from './ExtendedAccountProvider'
